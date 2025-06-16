ec879ba654a208b2e579b5be2e92f2313de7e049
DRIICE69 <driicecenter@gmail.com>
2025-06-16 16:28:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World DRX !');
+  res.send('Yo c'est DRIICE, tu captes où pas ?? 🤨 !');
 });
 
 module.exports = app;
