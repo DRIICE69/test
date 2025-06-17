e04133004a52aaa089fcc0216c8bc8cbf9669e01
DRIICE69 <driicecenter@gmail.com>
2025-06-17 10:37:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const logging = require('./logging');
+const checkOtp = require('./otp')
 
 const app = express();
 const id = "78f28e0c-915a-4dfc-9e95-0c52106af653";
@@ -8,6 +9,8 @@ const errors = {
   user: "user required",
   mail: "invalid or malformed mail"
 };
+
+//AUTH
 
 app.get('/user', (req, res) => {
   const user = req.query.user;
@@ -25,5 +28,26 @@ app.get('/user', (req, res) => {
       });
   }
 });
+
+//OTP
+
+app.get('/otp', (req, res) => {
+  const user = req.query.user;
+const code = req.query.otp
+  if (!user || !otp) {
+    return res.status(400).json({ error: errors.user });
+  } else {
+    // Authentification avec gestion de la promesse
+    checkOtp.check(user, code)
+      .then((response) => {
+        res.send(response)
+      })
+      .catch(error => {
+        console.error('Error in check:', error);
+        res.status(500).json({ error: "Internal server error" });
+      });
+  }
+});
+
 
 module.exports = app;
