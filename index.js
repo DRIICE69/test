e5c44a154c8d56a656c3989d3bf14df9959a7636
DRIICE69 <driicecenter@gmail.com>
2025-06-17 03:35:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-
 const express = require('express');
-//const logging = require('./logging');
+
+const logging = require('./logging');
 
 
 const app = express();
 
 const id = "78f28e0c-915a-4dfc-9e95-0c52106af653"
-// Correction de la structure d'erreur
+
 const errors = {
   user: "user required",
   mail: "invalid or malformed mail"
@@ -15,13 +15,13 @@ const errors = {
 app.get('/user', (req, res) => {
   const user = req.query.user;
   if (!user) {
-    // Correction de la réponse d'erreur
+   
     return res.status(400).json({ error: errors.user });
   } else {
-  
+//Authentification 
+  logging.logUser(user)
 
   }
 });
 
-// N'oubliez pas d'exporter l'app pour Vercel
 module.exports = app;
