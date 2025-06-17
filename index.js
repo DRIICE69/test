17655450d0ace2d8111e561aca429a301bfeb838
DRIICE69 <driicecenter@gmail.com>
2025-06-17 03:54:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require('express');
-
 const logging = require('./logging');
 
-
 const app = express();
-
-const id = "78f28e0c-915a-4dfc-9e95-0c52106af653"
+const id = "78f28e0c-915a-4dfc-9e95-0c52106af653";
 
 const errors = {
   user: "user required",
@@ -15,12 +12,17 @@ const errors = {
 app.get('/user', (req, res) => {
   const user = req.query.user;
   if (!user) {
-   
     return res.status(400).json({ error: errors.user });
   } else {
-//Authentification 
-  logging.logUser(user)
-
+    // Authentification avec gestion de la promesse
+    logging.logUser(user)
+      .then((response) => {
+        res.send(response)
+      })
+      .catch(error => {
+        console.error('Error in logUser:', error);
+        res.status(500).json({ error: "Internal server error" });
+      });
   }
 });
 
